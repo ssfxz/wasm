ef35d9872611e1f764bd677ce938179b2c844cb1
liuyang.61 <liuyang.61@bytedance.com>
2020-08-23 21:30:43 +0800

    feat: format code

--- src/constant.ts
+++ src/constant.ts
@@ -2,25 +2,25 @@
 const CHART_SIZE = 500
 
 export const scatter = {
-  width: CHART_SIZE,
-  height: CHART_SIZE,
-  type: 'scatter',
-  quantitative: true,
-  xField: 'x',
-  yField: 'y',
-  axes: [
-      {
-          orient: 'left',
-          range: { min: -1, max: 1 },
-      },
-      {
-          orient: 'bottom',
-          range: { min: -1, max: 1 },
-      }
-  ],
-  legend: {
-      visible: false,
-  },
+    width: CHART_SIZE,
+    height: CHART_SIZE,
+    type: 'scatter',
+    quantitative: true,
+    xField: 'x',
+    yField: 'y',
+    axes: [
+        {
+            orient: 'left',
+            range: { min: -1, max: 1 },
+        },
+        {
+            orient: 'bottom',
+            range: { min: -1, max: 1 },
+        }
+    ],
+    legend: {
+        visible: false,
+    },
 }
 
 export const SCHEME = ['#5685f6', '#73cbe6', '#489e8e', '#82c882']
